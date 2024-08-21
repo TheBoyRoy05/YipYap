67f28fd77478a30a65748354c59ec3511324443d
Issac Roy <issacroy05@gmail.com>
2024-08-21 13:04:06 +0530

    Feat: login and logout backend set up

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -77,6 +77,12 @@ export const login = async (req, res) => {
   }
 };
 
-export const logout = (req, res) => {
-  console.log("Logout");
+export const logout = async (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
