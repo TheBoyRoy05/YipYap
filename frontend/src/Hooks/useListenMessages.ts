a303cc792bdfbe1663ec2f20c424f4eb8aff7cde
Issac Roy <issacroy05@gmail.com>
2024-09-05 23:56:18 -0700

    Feat: Added Notification Sound

--- frontend/src/Hooks/useListenMessages.ts
+++ frontend/src/Hooks/useListenMessages.ts
@@ -1,22 +1,29 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from "react";
 import useSocket from "../Store/useSocket";
 import useStore from "../Store/useStore";
 import { MessageType } from "../Utils/Types";
+import MeowMeow from "../assets/Sounds/sad-meow-song.mp3";
 
 const useListenMessages = () => {
   const { socket } = useSocket();
   const { receiver, setMessages } = useStore();
 
   useEffect(() => {
+    const sound = new Audio(MeowMeow);
+
     socket?.on("newMessage", (message: MessageType) => {
-      if (receiver._id == message.receiverID)
+      if (receiver._id == message.senderID) {
         setMessages((messages) => [...messages, message]);
+      }
+      sound.currentTime = 0;
+      sound.play();
     });
 
     return () => {
       if (socket) socket.off("newMessage");
     };
-  }, [receiver._id, setMessages, socket]);
+  }, [socket, receiver._id]);
 };
 
 export default useListenMessages;
