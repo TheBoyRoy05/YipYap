95e89d955da035158835bcf4e0df4035054c6072
Issac Roy <issacroy05@gmail.com>
2024-09-06 12:20:49 -0700

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/authRoutes.js";
@@ -10,6 +11,7 @@ import connectDatabase from "./database/connect.js";
 import { app, server } from "./socket/socket.js";
 
 const port = process.env.PORT || 3000;
+const _dirname = path.resolve();
 
 dotenv.config();
 app.use(express.json());
@@ -18,6 +20,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/messages", messageRoutes);
+
+app.use(express.static(path.join(_dirname, "frontend/dist")));
+
+app.get(("*"), (req, res) => {
+  res.sendFile(path.join(_dirname, "frontend/dist/index.html"));
+});
 
 server.listen(port, () => {
   connectDatabase();
